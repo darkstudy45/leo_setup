40c7e25eecf2a98f63776f8e2a8c4834ce90bf85
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2024-01-09 17:42:52 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ sudo apt-get install libc6-dev -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo apt-get update
 sudo apt-get install pkg-config libssl-dev -y
-$HOME/.cargo/env
+source "$HOME/.cargo/env"
 sudo reboot
