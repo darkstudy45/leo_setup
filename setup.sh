050d592ec8b4ec5c0e18b55e651fc6426f71c8fb
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2024-01-09 10:25:29 +0600

    Update setup.sh
    fixed cc error

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
+sudo apt-get install build-essential -y
 sudo apt-get install git-all -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 $HOME/.cargo/env
