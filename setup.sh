3c409bba9bfd003e67b611ce6eb7df6e7eacbe49
darkstudy45 <114846213+darkstudy45@users.noreply.github.com>
2024-01-09 10:48:41 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install build-essential -y
 sudo apt-get install git-all -y
+sudo apt-get install libc6-dev -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt-get update
+sudo apt-get install pkg-config libssl-dev -y
 $HOME/.cargo/env
 sudo reboot
